21c186e3e991312b1848c3d475cdb562d687c026
MiroMaric <miromaric1996@gmail.com>
2017-04-02 03:59:34 +0200

    Dodata dokumentacija za klasu Twitter kao i za atribute i metode u toj klasi. paket:com.twitter

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -2,14 +2,28 @@ package com.twitter;
 
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * JUnit and javadoc -domaci zadatak 2017
+ * @author MiroMaric
+ * @version 2.0
+ *
+ */
 public class Twitter {
+	/**
+	 * Lista poruka koje su poslali korisnici
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+	/**
+	 * @return listu poruka koju su poslali korisnici
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+	/**
+	 * Metoda koja unosi novu poruku u listu poruka korisnika
+	 * @param korisnik korisnicko ime
+	 * @param poruka poruka korisnika
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -18,7 +32,13 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	/**
+	 * Metoda koja vraca poruke u vidu niza koje sadrza odredjen tag
+	 * @param maxBroj poruka koje mogu sadrzati zadati tag
+	 * @param tag dati tag koji se proverava u datim porukama korisnika
+	 * @throws java.lang.RuntimeException ukoliko je tag pogresno unet
+	 * @return niz poruka sa zadatim tagom
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
